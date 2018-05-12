17de7940029da3144a3b418c03b73dceafb7299d
Laura Lucca <laura.lucca@buscapecompany.com>
2018-05-12 14:23:58 -0300

    melhorando o helper para manipulaçao de datas

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -5,14 +5,13 @@ class DateHelper {
     }
 
     static textoParaData(texto) {
-        return new Date(...texto
-            .split('-')
-            .map((item, indice) => item - indice % 2));
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('A data deve estar no formato aaaa-mm-dd');
+
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 
     static dataParaTexto(data) {
-        return data.getDate()
-            + '/' + (data.getMonth() + 1)
-            + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
 }
