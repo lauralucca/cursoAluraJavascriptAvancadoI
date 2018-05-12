d8fd53ac9681d90c895635edd0cf9afa231d165b
Laura Lucca <laura.lucca@buscapecompany.com>
2018-05-12 14:04:24 -0300

    criando um helper com métodos estáticos

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,18 +9,12 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
         
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map((item, indice) => item - indice % 2)
-        );
-
         let negociacao = new Negociacao(
-            data,
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
         console.log(negociacao);
-        console.log(data);
+        console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 }
